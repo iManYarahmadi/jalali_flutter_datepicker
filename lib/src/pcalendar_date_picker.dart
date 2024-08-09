edc3d8d3c1577604af778444e5a60ef54bbc448f
Mohammad Keshtehgar <mk.qeshmair@gmail.com>
2024-08-09 15:27:32 +0330

    debug print initialDate.month

--- lib/src/pcalendar_date_picker.dart
+++ lib/src/pcalendar_date_picker.dart
@@ -164,6 +164,7 @@ class _CalendarDatePickerState extends State<PCalendarDatePicker> {
     _currentDisplayedMonthDate =
         Jalali(widget.initialDate.year, widget.initialDate.month);
     _selectedDate = widget.initialDate;
+    print('${widget.initialDate.month}');
   }
 
   @override
